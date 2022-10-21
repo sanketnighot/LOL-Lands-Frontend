62983b194e8113cc7b5114b980bdbe8f0cdca7ca
sanketnighot <sanketnighot25@gmail.com>
2022-10-21 21:06:15 +0530

    update

--- components/Map.js
+++ components/Map.js
@@ -71,6 +71,32 @@ function Map({ lands, loading }) {
   const handleDrawerToggle = () => {
     setMobileOpen(!mobileOpen);
   };
+  // Set the date we're counting down to
+  var countDownDate =  new Date("Oct 21, 2022 17:00:00 UTC").getTime()
+  // Update the count down every 1 second
+  var x = setInterval(function() {
+  
+    // Get today's date and time
+    var now = Date.parse(new Date().toUTCString())
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+  
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  
+    // Display the result in the element with id="demo"
+    document.getElementById("demo").innerHTML = "Mint starts in " + days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s ";
+  
+    // If the count down is finished, write some text
+    if (distance < 0) {
+      clearInterval(x);
+      document.getElementById("demo").innerHTML = "Mint is Live";
+    }
+  }, 1000);
   const drawer = (
     <div>
       <Toolbar sx={{ background: "#0a213d" }}>
@@ -238,6 +264,13 @@ function Map({ lands, loading }) {
             <Typography variant="h7" noWrap component="div" sx={{ color: "white" }}>
               PLEASE DO NOT REFRESH THE PAGE
             </Typography>
+          </Box>
+          <Box sx={{ display: "flex", alignItems: "center" }}>
+            <Typography id="demo" variant="h5" noWrap component="div" sx={{ color: "white" }}>
+              
+            </Typography>
+          </Box>
+          <Box sx={{ display: "flex", alignItems: "center" }}>
           </Box>
           {/* <Button
             sx={{
