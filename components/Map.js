8d42d8520656916fb7d255ff91218b53427c5830
sanketnighot <sanketnighot25@gmail.com>
2022-10-22 21:45:55 +0530

    updating navigation and index page

--- components/Map.js
+++ components/Map.js
@@ -71,32 +71,32 @@ function Map({ lands, loading }) {
   const handleDrawerToggle = () => {
     setMobileOpen(!mobileOpen);
   };
-  // Set the date we're counting down to
-  var countDownDate =  new Date("Oct 21, 2022 17:00:00 UTC").getTime()
-  // Update the count down every 1 second
-  var x = setInterval(function() {
+  // // Set the date we're counting down to
+  // var countDownDate =  new Date("Oct 21, 2022 17:00:00 UTC").getTime()
+  // // Update the count down every 1 second
+  // var x = setInterval(function() {
   
-    // Get today's date and time
-    var now = Date.parse(new Date().toUTCString())
-    // Find the distance between now and the count down date
-    var distance = countDownDate - now;
+  //   // Get today's date and time
+  //   var now = Date.parse(new Date().toUTCString())
+  //   // Find the distance between now and the count down date
+  //   var distance = countDownDate - now;
   
-    // Time calculations for days, hours, minutes and seconds
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  //   // Time calculations for days, hours, minutes and seconds
+  //   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  //   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  //   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  //   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
   
-    // Display the result in the element with id="demo"
-    document.getElementById("demo").innerHTML = "Mint starts in " + days + "d " + hours + "h "
-    + minutes + "m " + seconds + "s ";
+  //   // Display the result in the element with id="demo"
+  //   document.getElementById("demo").innerHTML = "Mint starts in " + days + "d " + hours + "h "
+  //   + minutes + "m " + seconds + "s ";
   
-    // If the count down is finished, write some text
-    if (distance < 0) {
-      clearInterval(x);
-      document.getElementById("demo").innerHTML = "Mint is Live";
-    }
-  }, 1000);
+  //   // If the count down is finished, write some text
+  //   if (distance < 0) {
+  //     clearInterval(x);
+  //     document.getElementById("demo").innerHTML = "Mint is Live";
+  //   }
+  // }, 1000);
   const drawer = (
     <div>
       <Toolbar sx={{ background: "#0a213d" }}>
@@ -261,8 +261,8 @@ function Map({ lands, loading }) {
             </Typography>
           </Box> */}
           <Box sx={{ display: "flex", alignItems: "center" }}>
-            <Typography variant="h7" noWrap component="div" sx={{ color: "white" }}>
-              PLEASE DO NOT REFRESH THE PAGE
+            <Typography variant="h10" noWrap component="div" sx={{ color: "white" }}>
+              PLEASE DO NOT REFRESH THE PAGE AFTER CONFIRMING TRANSACTION ON METAMASK
             </Typography>
           </Box>
           <Box sx={{ display: "flex", alignItems: "center" }}>
